b8181f3e378c44b98cd1327fec1ae9c69018273f
kittulal <punnakrishnaa@gmail.com>
2026-01-16 07:07:58 +0530

    first commit

---
+++ src/main/java/com/krish/app/jpa/AppSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.krish.app.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringDataJpaApplication.class, args);
+	}
+
+}
